c6dfc64d7c79ec078f256425d2270dee8b9cc487
Goy <saiphanas.k@gmail.com>
2023-12-18 21:50:36 +0700

    feat: Add new task button logic

--- app/components/AddTask.tsx
+++ app/components/AddTask.tsx
@@ -3,23 +3,23 @@ import { AiOutlinePlus } from 'react-icons/ai';
 import Modal from './Modal';
 import { FormEventHandler, useState } from 'react';
 import { addTodo } from '@/api';
-import { useRouter } from 'next/router';
-// import { v4 as uuidv4 } from 'uuid';
+import { useRouter } from 'next/navigation';
+import { v4 as uuidv4 } from 'uuid';
 
 const AddTask = () => {
-  // const router = useRouter();
+  const router = useRouter();
   const [modalOpen, setModalOpen] = useState<boolean>(false);
   const [newTaskValue, setNewTaskValue] = useState<string>('');
 
   const handleSubmitNewTodo: FormEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault();
     await addTodo({
-      id: '3',
+      id: uuidv4,
       text: newTaskValue,
     });
     setNewTaskValue('');
     setModalOpen(false);
-    // router.refresh();
+    router.refresh();
   };
 
   return (
