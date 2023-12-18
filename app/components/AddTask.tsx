4fc87064cbe5d34b1caa438641109bc8fcc22c63
Goy <saiphanas.k@gmail.com>
2023-12-18 15:19:51 +0700

    feat: add Confirmation modal component on landing page

--- app/components/AddTask.tsx
+++ app/components/AddTask.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
+'use client';
 import { AiOutlinePlus } from 'react-icons/ai';
+import Modal from './Modal';
+import { useState } from 'react';
 
 const AddTask = () => {
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
   return (
     <div>
-      <button className="btn btn-primary w-full">
-        {' '}
+      <button
+        onClick={() => setModalOpen(true)}
+        className="btn btn-primary w-full"
+      >
         Add New Task <AiOutlinePlus className="ml-2" size={15} />
       </button>
+
+      <Modal modalOpen={modalOpen} />
     </div>
   );
 };
